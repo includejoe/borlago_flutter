de4eb88c95897534b3fedd5e8b1c8c663fcb547b
joel ayertey <kvnqjoe@gmail.com>
2023-06-06 21:21:52 +0000

    mc

--- lib/base/main.dart
+++ lib/base/main.dart
@@ -13,12 +13,11 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'presentation/widgets/main_page_view.dart';
 
-// TODO: + change password request
+// TODO: + create wcr request
+// TODO: + get user wcrs request
 // TODO: + locations screen ui
 // TODO: + add location request
 // TODO: + payments screen ui
-// TODO: + create wcr request
-// TODO: + get user wcrs request
 // TODO: fix camera flashlight bug
 
 
